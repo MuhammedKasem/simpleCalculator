6d50ef829ef29f770ea9a607052aa3cc6245d835
Muhammed Kasem <muhammedkasem2002@gmail.com>
2022-08-24 22:46:01 -0400

    Fixed bugs that would provide incorrect numbers

--- script.js
+++ script.js
@@ -10,39 +10,39 @@ let displayText = "";
 let operation = "";
 let result = 0.0;
 
-function add (num1, num2) {
+function add(num1, num2) {
   return parseFloat(num1) + parseFloat(num2);
 }
 
-function subtract (num1, num2) {
+function subtract(num1, num2) {
   return parseFloat(num1) - parseFloat(num2);
 }
 
-function multiply (num1, num2) {
+function multiply(num1, num2) {
   return parseFloat(num1) * parseFloat(num2);
 }
 
-function divide (num1, num2) {
+function divide(num1, num2) {
   return parseFloat(num1) / parseFloat(num2);
 }
 
 function operate(num1, num2) {
-  switch(operation) {
-  case "+":
-    result = add(num1, num2);
-    break;
-  
-  case "-":
-    result = subtract(num1, num2);
-    break;
+  switch (operation) {
+    case "+":
+      result = add(num1, num2);
+      break;
 
-  case "x":
-    result = multiply(num1, num2);
-    break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
 
-  case "/":
-    result = divide(num1, num2);
-    break;
+    case "x":
+      result = multiply(num1, num2);
+      break;
+
+    case "/":
+      result = divide(num1, num2);
+      break;
   }
   return result;
 }
@@ -53,18 +53,22 @@ digits.forEach((digit) => {
 
 operations.forEach((operationBtn) => {
   operationBtn.addEventListener('click', setDisplay)
-  })
+})
 
 
 function setDisplay(e) {
+  if (screen.textContent == 0) screen.textContent = "";
   screen.textContent += e.target.innerHTML
   if (e.target.classList.contains('operation')) {
-    if (num1 !== "" && num2!== "") { num1 = operate(num1, num2); num2 = "" };
+    if (num1 !== "" && num2 !== "") {
+      num1 = operate(num1, num2);
+      num2 = "";
+      screen.textContent = num1;
+      screen.textContent += e.target.innerHTML;
+    };
     operation = e.target.innerHTML;
-      console.log(num1);
-      console.log(num2);
-      return;
-    }
+    return;
+  }
   (operation == "") ? num1 += e.target.innerHTML : num2 += e.target.innerHTML;
 }
 
@@ -74,8 +78,8 @@ function clearCalc() {
   operation = "";
   result = 0;
 }
-clearBtn.addEventListener('click', () => { screen.textContent = "0"; clearCalc();})
-equalsBtn.addEventListener('click', () => { 
+clearBtn.addEventListener('click', () => { screen.textContent = "0"; clearCalc(); })
+equalsBtn.addEventListener('click', () => {
   result = operate(num1, num2);
   screen.textContent += ' = ' + result;
   operation = "";
